908c2cdaa64fe4712aee2d254c513503c1cf9378
mainprograming <63953689+mainprograming@users.noreply.github.com>
2020-09-13 22:07:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,17 @@ $(function () {
   });
 
   //上スクロールでheaderが消える//
-
   //下スクロールでheaderが出てくる//
+  var headerHeight = $('header').outerHeight(),
+		startPos = 0;
+	$(window).on('load scroll', function() {
+		var scrollPos = $(this).scrollTop();
+		if ( scrollPos > startPos && scrollPos > headerHeight ) {
+			$('header').css('top', '-' + headerHeight + 'px');
+		} else {
+			$('header').css('top', '0');
+		}
+		startPos = scrollPos;
+	});
 
 });
