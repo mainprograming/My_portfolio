1173465fa475f0914ce5effc30747f52814cf141
mainprograming <63953689+mainprograming@users.noreply.github.com>
2020-09-16 14:13:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,52 @@
 $(function () {
 
   // #で始まるアンカーをクリックした場合に処理
-  $('header a[href^=#]').click(function(){
+  $('header a[href^=#]').click(function () {
     // 移動先を50px上にずらす
     var adjust = 50;
     // スクロールの速度
     var speed = 400; // ミリ秒
     // アンカーの値取得
-    var href= $(this).attr("href");
+    var href = $(this).attr("href");
     // 移動先を取得
     var target = $(href == "#" || href == "" ? 'html' : href);
     // 移動先を調整
     var position = target.offset().top - adjust;
     // スムーススクロール
-    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    $('body,html').animate({ scrollTop: position }, speed, 'swing');
     return false;
   });
 
   //上スクロールでheaderが消える//
   //下スクロールでheaderが出てくる//
   var headerHeight = $('header').outerHeight(),
-		startPos = 0;
-	$(window).on('load scroll', function() {
-		var scrollPos = $(this).scrollTop();
-		if ( scrollPos > startPos && scrollPos > headerHeight ) {
-			$('header').css('top', '-' + headerHeight + 'px');
-		} else {
-			$('header').css('top', '0');
-		}
-		startPos = scrollPos;
-	});
+    startPos = 0;
+  $(window).on('load scroll', function () {
+    var scrollPos = $(this).scrollTop();
+    if (scrollPos > startPos && scrollPos > headerHeight) {
+      $('header').css('top', '-' + headerHeight + 'px');
+    } else {
+      $('header').css('top', '0');
+    }
+    startPos = scrollPos;
+  });
 
-	//スクロールで色々出てくる//
+  //スクロールで色々出てくる//
   ScrollReveal().reveal('.scroll', {
-     duration: '1500', reset: 'true'
-    });
+    duration: '1500',
+    reset: 'true'
+  });
+  ScrollReveal().reveal('.scroll-reft', {
+    duration: 1600,
+    origin: 'left',
+    distance: '50px',
+    reset: true
+  });
+  ScrollReveal().reveal('.scroll-right', {
+    duration: 1600,
+    origin: 'right',
+    distance: '50px',
+    reset: true
+  });
 });
+
